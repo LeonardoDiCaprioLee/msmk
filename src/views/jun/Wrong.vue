e7db93510fbca967c156b904ea79d1254c1de071
2c <1054711110@qq.com>
2021-01-15 17:08:21 +0800

    学习页面完成

---
+++ src/views/jun/Wrong.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <van-nav-bar title="错题记录" left-text="返回" @click-left="$router.go(-1)" left-arrow>
+      <template #right>
+        <van-icon name="search" size="18" />
+      </template>
+    </van-nav-bar>
+    <van-dropdown-menu>
+      <van-dropdown-item v-model="value1" :options="option1" />
+      <van-dropdown-item v-model="value2" :toggle="false" :options="option2" />
+      <van-dropdown-item v-model="value3" :options="option2" />
+    </van-dropdown-menu>
+    <div class="Wrong">
+      <div class="Wrong-div" style="margin-top:0.8rem">
+        <span>物理学史</span>
+        <span style="color:#0088dd;">7道错题</span>
+      </div>
+      <p style="margin-left:1.3rem;margin-top:0.5rem;color: #b7b7b7">提交时间:2020/10/28 15:16:53</p>
+      <button class="Wrong-but" @click="$router.push('/analysis')">巩固错题</button>
+    </div>
+    <p style="font-size:0.24rem;text-align:center">没有更多了</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "",
+  props: {},
+  data() {
+    return {
+      value1: 0,
+      value2: 0,
+      value3: 0,
+      option1: [
+        { text: "全部商品", value: 0 },
+        { text: "新款商品", value: 1 },
+        { text: "活动商品", value: 2 }
+      ],
+      option2: [{ text: "巩固错题", value: 0 }],
+      option3: [{ text: "移除错题", value: 0 }]
+    };
+  },
+  mounted() {},
+  components: {},
+  computed: {},
+  methods: {}
+};
+</script>
+
+<style scoped>
+</style>
--- src/views/xiang/Exercise.vue
+++ src/views/xiang/Exercise.vue
@@ -1,169 +1,177 @@
 <template>
-    <div class='exercise'>
-        <van-nav-bar
-        title="练习"
-        />
-        <div class="sex">
-            <ul>
-                <li>
-                    <div><img src="../../assets/images/exercise-img/1-1.png" alt=""></div>
-                    <p>考点专练</p>
-                </li>
-                <li>
-                    <div><img src="../../assets/images/exercise-img/1-2.png" alt=""></div>
-                    <p>考卷练习</p>
-                </li>
-                <li>
-                    <div><img src="../../assets/images/exercise-img/1-3.png" alt=""></div>
-                    <p>仿真模拟</p>
-                </li>
-                <li>
-                    <div><img src="../../assets/images/exercise-img/1-4.png" alt=""></div>
-                    <p>错题练习</p>
-                </li>
-                <li>
-                    <div><img src="../../assets/images/exercise-img/1-5.png" alt=""></div>
-                    <p>测评记录</p>
-                </li>
-                <li>
-                    <div><img src="../../assets/images/exercise-img/1-6.png" alt=""></div>
-                    <p>习题收藏</p>
-                </li>
-            </ul>
-        </div>
-        <div class="content">
-            <p class="one">近期模考</p>
-            <p class="tow">
-                <span  @click="show1" :class="show==true?'color':''">默认排序</span>
-                <span>
-                    <span @click="show2" :class="show==false?'color':''" class="time">时间</span>
-                    <span v-show="!arrows==''">
-                        <img v-if="arrows==false" src="../../assets/images/exercise-img/top.png" alt="">
-                        <img v-else-if="arrows==true" src="../../assets/images/exercise-img/bottom.png" alt="">
-                    </span>
-                </span>
-            </p>
-            <div class="no">
-                <img src="../../assets/images/exercise-img/max.png" alt="">
-                <p>暂无模考安排，敬请期待</p>
-            </div>
-        </div>
+  <div class="exercise">
+    <van-nav-bar title="练习" />
+    <div class="sex">
+      <ul>
+        <li  @click="$router.push('/site')">
+          <div>
+            <img src="../../assets/images/exercise-img/1-1.png" alt />
+          </div>
+          <p>考点专练</p>
+        </li>
+        <li>
+          <div>
+            <img src="../../assets/images/exercise-img/1-2.png" alt />
+          </div>
+          <p>考卷练习</p>
+        </li>
+        <li>
+          <div>
+            <img src="../../assets/images/exercise-img/1-3.png" alt />
+          </div>
+          <p>仿真模拟</p>
+        </li>
+        <li @click="$router.push('/Wrong')">
+          <div>
+            <img src="../../assets/images/exercise-img/1-4.png" alt />
+          </div>
+          <p>错题练习</p>
+        </li>
+        <li @click="$router.push('/Wrong')">
+          <div>
+            <img src="../../assets/images/exercise-img/1-5.png" alt />
+          </div>
+          <p>测评记录</p>
+        </li>
+        <li @click="$router.push('/Collection')">
+          <div>
+            <img src="../../assets/images/exercise-img/1-6.png" alt />
+          </div>
+          <p>习题收藏</p>
+        </li>
+      </ul>
     </div>
+    <div class="content">
+      <p class="one">近期模考</p>
+      <p class="tow">
+        <span @click="show1" :class="show==true?'color':''">默认排序</span>
+        <span>
+          <span @click="show2" :class="show==false?'color':''" class="time">时间</span>
+          <span v-show="!arrows==''">
+            <img v-if="arrows==false" src="../../assets/images/exercise-img/top.png" alt />
+            <img v-else-if="arrows==true" src="../../assets/images/exercise-img/bottom.png" alt />
+          </span>
+        </span>
+      </p>
+      <div class="no">
+        <img src="../../assets/images/exercise-img/max.png" alt />
+        <p>暂无模考安排，敬请期待</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            show:true,
-            arrows:''
-        };
+  data() {
+    return {
+      show: true,
+      arrows: ""
+    };
+  },
+  mounted() {},
+  methods: {
+    show1() {
+      this.show = true;
+      this.arrows = false;
     },
-    mounted() {},
-    methods: {
-        show1(){
-            this.show = true
-            this.arrows = false
-            
-        },
-        show2(){
-            this.show = false
-            if(this.arrows == ''){
-                return this.arrows = true
-            } else {
-                this.arrows = !this.arrows
-            }
-        }
-    },
+    show2() {
+      this.show = false;
+      if (this.arrows == "") {
+        return (this.arrows = true);
+      } else {
+        this.arrows = !this.arrows;
+      }
+    }
+  }
 };
 </script>
 
 <style lang='scss' scoped>
-    .exercise{
-        width: 100%;
-        height: 100%;
-        background-color: #f0f2f5;
-        .van-nav-bar__title{
-            font-size: 0.36rem;
+.exercise {
+  width: 100%;
+  height: 100%;
+  background-color: #f0f2f5;
+  .van-nav-bar__title {
+    font-size: 0.36rem;
+  }
+  .sex {
+    width: 100%;
+    ul {
+      width: 7.1rem;
+      height: 100%;
+      background-color: #fff;
+      border-radius: 0.1rem;
+      display: flex;
+      flex-wrap: wrap;
+      margin: 0.2rem;
+      padding: 0.1rem 0;
+      li {
+        width: 25%;
+        height: 1.68rem;
+        padding-top: 0.2rem;
+        div {
+          width: 0.8rem;
+          height: 0.8rem;
+          border-radius: 50%;
+          box-shadow: 0 0 15px -10px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          margin: 0 0.48rem;
+          img {
+            width: 0.32rem;
+          }
         }
-        .sex{
-            width: 100%;
-            ul{
-                width: 7.1rem;
-                height: 100%;
-                background-color: #fff;
-                border-radius: 0.1rem;
-                display: flex;
-                flex-wrap: wrap;
-                margin: 0.2rem;
-                padding: 0.1rem 0;
-                li{
-                    width: 25%;
-                    height: 1.68rem;
-                    padding-top: 0.2rem;
-                    div{
-                        width: 0.8rem;
-                        height: 0.8rem;
-                        border-radius: 50%;
-                        box-shadow: 0 0 15px -10px;
-                        display: flex;
-                        justify-content: center;
-                        align-items: center;
-                        margin: 0 0.48rem;
-                        img{
-                            width: 0.32rem;
-                        }
-                    }
-                    p{
-                        width: 100%;
-                        height: 0.68rem;
-                        font-size: 0.24rem;
-                        text-align: center;
-                        line-height: 0.68rem;
-                        color: #595959;
-                    }
-                }
-            }
+        p {
+          width: 100%;
+          height: 0.68rem;
+          font-size: 0.24rem;
+          text-align: center;
+          line-height: 0.68rem;
+          color: #595959;
         }
-        .content{
-            width: 100%;
-            background-color: #fff;
-            padding: 0.3rem;
-            .one{
-                width: 100%;
-                font-size: 0.3rem;
-                margin-bottom: 0.29rem;
-                color: #8c8c8c;
-            }
-            .tow{
-                width: 100%;
-                display: flex;
-                justify-content: space-between;
-                margin-bottom: 0.45rem;
-                font-size: 0.26rem;
-                .time{
-                    margin-right: 0.1rem;
-                }
-            }
-            .color{
-                color: #eb6100;
-            }
-                .no{
-                width: 100%;
-                height: 2.92rem;
-                text-align: center;
-                img{
-                    width: 1.76rem;
-                    margin-bottom: 0.3rem;
-                }
-                p{
-                    width: 100%;
-                    font-size: 0.24rem;
-                    color: #8c8c8c;
-                    text-align: center;
-                }
-            }
-        }
-        
+      }
     }
+  }
+  .content {
+    width: 100%;
+    background-color: #fff;
+    padding: 0.3rem;
+    .one {
+      width: 100%;
+      font-size: 0.3rem;
+      margin-bottom: 0.29rem;
+      color: #8c8c8c;
+    }
+    .tow {
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+      margin-bottom: 0.45rem;
+      font-size: 0.26rem;
+      .time {
+        margin-right: 0.1rem;
+      }
+    }
+    .color {
+      color: #eb6100;
+    }
+    .no {
+      width: 100%;
+      height: 2.92rem;
+      text-align: center;
+      img {
+        width: 1.76rem;
+        margin-bottom: 0.3rem;
+      }
+      p {
+        width: 100%;
+        font-size: 0.24rem;
+        color: #8c8c8c;
+        text-align: center;
+      }
+    }
+  }
+}
 </style>
