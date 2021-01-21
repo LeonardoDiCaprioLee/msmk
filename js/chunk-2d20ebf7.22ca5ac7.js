2e14efd69a125cbaf877737c9f658cd53cc7cb7a
lixiang <1140393415@qq.com>
2021-01-21 10:59:34 +0800

    打包

---
+++ js/chunk-2d20ebf7.22ca5ac7.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d20ebf7"],{b14f:function(t,n,e){"use strict";e.r(n);var o=function(){var t=this,n=t.$createElement,e=t._self._c||n;return e("div",{staticClass:"one_to_one"},[e("van-nav-bar",{attrs:{title:"剩余学习币","left-arrow":""},on:{"click-left":t.onClickLeft}})],1)},c=[],a={data:function(){return{}},mounted:function(){},methods:{onClickLeft:function(){this.$router.go(-1)}}},i=a,l=e("2877"),r=Object(l["a"])(i,o,c,!1,null,null,null);n["default"]=r.exports}}]);
+//# sourceMappingURL=chunk-2d20ebf7.22ca5ac7.js.map
---
+++ js/chunk-2d2263c6.304e6576.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d2263c6"],{e896:function(t,n,e){"use strict";e.r(n);var o=function(){var t=this,n=t.$createElement,e=t._self._c||n;return e("div",{staticClass:"one_to_one"},[e("van-nav-bar",{attrs:{title:"一对一辅导","left-arrow":""},on:{"click-left":t.onClickLeft}})],1)},c=[],a={data:function(){return{}},mounted:function(){},methods:{onClickLeft:function(){this.$router.go(-1)}}},i=a,l=e("2877"),r=Object(l["a"])(i,o,c,!1,null,null,null);n["default"]=r.exports}}]);
+//# sourceMappingURL=chunk-2d2263c6.304e6576.js.map
